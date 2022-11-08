175de0c202779616c68c58d6126d1b4106718bdc
karthick <vkbalganesh@gmail.com>
2022-11-08 14:37:43 +0530

    Private Route Added

---
+++ loginpage/src/Routes/PrivateRoute.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Outlet,Navigate } from "react-router-dom";
+function PrivateRoute({authLoading}){
+  return authLoading?<Outlet/>:<Navigate to='/'/>;
+}
+
+
+export default PrivateRoute;
